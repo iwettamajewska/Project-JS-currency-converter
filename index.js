1d14e1abe0087d03fa4cf3d1990f23e881e4b1d7
iwetta <iwetta.majewska@gmail.com>
2022-09-13 17:15:52 +0200

    zmiany

--- index.js
+++ index.js
@@ -7,18 +7,24 @@ const outOfmoney = document.querySelector('.result-pln');
 // const urlEur = 'http://api.nbp.pl/api/exchangerates/rates/a/eur/';
 // const urlUsd = 'http://api.nbp.pl/api/exchangerates/rates/a/usd/';
 // const urlChf = 'http://api.nbp.pl/api/exchangerates/rates/a/chf/';
-const url = 'http://api.nbp.pl/api/exchangerates/rates/a/';
+const url = 'https://api.nbp.pl/api/exchangerates/tables/A/?format=json';
 const formSelectList = document.querySelectorAll('.form-select option-rates');
 const availablecurrencies = ['EUR', 'USD', 'CHF'];
+let newArr;
 
 btn.addEventListener('click', () => {
     fetch(url)
     .then((response) => response.json())
-    .then((response) => availablecurrencies.map(element => response.data[0].rates.includes(element)))
-    .then((data) => console.log(data))
+    .then((data) => newArr = data[0].rates.filter(element => availablecurrencies.includes(element.code)))
+    .then(() => u co Bartek mi mowil)
     .catch((err) => console.log('err', err));
+    console.log(newArr);
 })
 
+
+
+
+// .then((response) => availablecurrencies.map(element => response.data[0].rates.includes(element)))
 
 // data['rates']
 

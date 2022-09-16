cf8c48d399356917c0d7704e1bfcc3c4f3fef01f
iwetta <iwetta.majewska@gmail.com>
2022-09-16 23:50:42 +0200

    zmiany

--- index.js
+++ index.js
@@ -10,17 +10,31 @@ const outOfmoney = document.querySelector('.result-pln');
 const url = 'https://api.nbp.pl/api/exchangerates/tables/A/?format=json';
 const formSelectList = document.querySelectorAll('.form-select option-rates');
 const availablecurrencies = ['EUR', 'USD', 'CHF'];
-let newArr;
+let threeCurrencies;
 
 btn.addEventListener('click', () => {
     fetch(url)
     .then((response) => response.json())
-    .then((data) => newArr = data[0].rates.filter(element => availablecurrencies.includes(element.code)))
-    .then(() => u co Bartek mi mowil)
+    .then((data) => threeCurrencies = data[0].rates.filter(element => availablecurrencies.includes(element.code)))
+    // .then(() => console.log(threeCurrencies[0]))   
+    // .then(() => outOfmoney.value = inputOfmoney.value *  )                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  )
     .catch((err) => console.log('err', err));
-    console.log(newArr);
+    console.log(threeCurrencies);
 })
 
+
+// tutaj (w .then) 
+// mam dostęp do threeCurrencies który jest uzupełniony danymi z BE. 
+// Tutaj muszę wywołać jakąś funckję, do której przekaże tego threeCurrencies 
+// ALBO po prostu wywołać funckję, która co teraz zrobi? Będzie musiała stworzyć 3 optiony, które będą miały jakieś value równe wartości tej waluty.
+
+
+// .then((data) => {
+//     const entries = Object.entries(data);
+//     for(i=0; i <entries.length; i++){
+//         select[0].innerHTML = `<option value="${entries[i][0]}">${entries[i][0]}`
+//     }
+// }
 
 
 

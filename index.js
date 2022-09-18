3d99471f9666aa1af5ee8590c714c8cbf56fb947
iwetta <iwetta.majewska@gmail.com>
2022-09-18 22:13:09 +0200

    zmiany

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const selector = document.querySelector('.form-select');
 const availablecurrencies = ['EUR', 'USD', 'CHF'];
 let threeCurrencies;
 
+const urltwo = 'http://api.nbp.pl/api/exchangerates/rates/a/chf/';
+const currencyConverterBtn = document.querySelector('.currency-converter-btn');
 
 btn.addEventListener('click', () => {
     fetch(url)
@@ -19,3 +21,40 @@ btn.addEventListener('click', () => {
     .catch((err) => console.log('err', err))
     })
 
+
+    // currencyConverterBtn.addEventListener('click', () => {           
+    //         const newRow = document.createElement('div');
+    //          newRow.classList.add('row');
+    //          newRow.classList.add('bottom-row');
+            
+    //          const newElement = document.createElement('span');
+    //          newElement.classList.add('live-exchange-rates');
+    //          newElement.classList.add('col-6');
+    //          newRow.appendChild(newElement);
+
+    // })
+
+    
+
+    console.log(urltwo)
+
+    currencyConverterBtn.addEventListener('click', () => {
+        fetch(urltwo)
+        .then((response) => response.json())
+        .then((urltwo) => console.log(urltwo))
+        .then((data) => {
+            chf = data[0].rate.mid;
+            console.log(chf);
+        })
+        .catch((err) => console.error('err', err));
+        })
+
+
+        function get_emoji(emoji) {
+            let emoji_code = [...emoji].map(e => e.codePointAt(0).toString(16)).join(`-`).toUpperCase();
+            new_url = `https://openmoji.org/data/color/svg/${emoji_code}.svg`
+            document.write(`<img src=${new_url} style="height: 80px;">`);
+        }
+        get_emoji("🦴")
+        get_emoji("🎭")
+        get_emoji("👩‍⚕️")

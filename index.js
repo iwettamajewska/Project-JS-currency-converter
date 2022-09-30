a2787059db03b960edeaaf512e99941300e4e515
iwetta <iwetta.majewska@gmail.com>
2022-10-01 00:30:38 +0200

    zmiany

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const inputOfmoney = document.querySelector('.amount-of-money');
 const outOfmoney = document.querySelector('.result-pln');
 const selector = document.querySelector('.form-select');
 const availablecurrencies = ['EUR', 'USD', 'CHF'];
+
 let threeCurrencies;
 
 btn.addEventListener('click', () => {
@@ -13,7 +14,7 @@ btn.addEventListener('click', () => {
         threeCurrencies = data[0].rates.filter(element => availablecurrencies.includes(element.code))
         const selectValue = selector.value;
         const mid = threeCurrencies.find(element => element.code === selectValue).mid;
-        outOfmoney.innerText = Number.parseFloat(inputOfmoney.value * mid).toFixed(2)
+        outOfmoney.value = Number.parseFloat(inputOfmoney.value * mid).toFixed(2)
         // Druga mozliwosc (zostaw sobie, ale bez wysyłania do sprawdzenia):
         // let result = inputOfmoney.value * mid;
         // result = Number.parseFloat(result).toFixed(2);
@@ -21,45 +22,46 @@ btn.addEventListener('click', () => {
     .catch((err) => console.log('err', err))
     })
 
-
-    // currencyConverterBtn.addEventListener('click', () => {           
-    //         const newRow = document.createElement('div');
-    //          newRow.classList.add('row');
-    //          newRow.classList.add('bottom-row');
-            
-    //          const newElement = document.createElement('span');
-    //          newElement.classList.add('live-exchange-rates');
-    //          newElement.classList.add('col-6');
-    //          newRow.appendChild(newElement);
-
-    // })
-
-    const urltwo = 'http://api.nbp.pl/api/exchangerates/rates/a/chf/';
+    
+    
     const currencyConverterBtn = document.querySelector('.currency-converter-btn');
 
+    const urlEffectiveDate = 'http://api.nbp.pl/api/exchangerates/tables/A/';
 
-
-    console.log(urltwo)
-
-    currencyConverterBtn.addEventListener('click', () => {
-        fetch(urltwo)
-        .then((response) => response.json())
-        .then((urltwo) => console.log(urltwo))
-        .then((data) => {
-            chf = data[0].rate.mid;
-            console.log(chf);
-        })
-        .catch((err) => console.error('err', err));
-        })
-
+    
+    // const showEffectiveDate = () => {
+    //     fetch(urlEffectiveDate)
+    //      .then((response) => response.json())
+    //        .then((data) => {
+    //          let effectiveDate = document.querySelector('.effective-date');
+    //          effectiveDate.innerHTML = data[0].effectiveDate;
+    //         // document.effectiveDate.insertAdjacentHTML(`<span>${data.value}</span>`);
+    //  })
+    //        .catch((err) => console.error('err', err));
+    //        }
 
 
 
-        function get_emoji(emoji) {
-            let emoji_code = [...emoji].map(e => e.codePointAt(0).toString(16)).join(`-`).toUpperCase();
-            new_url = `https://openmoji.org/data/color/svg/${emoji_code}.svg`
-            document.write(`<img src=${new_url} style="height: 80px;">`);
-        }
-        get_emoji("🦴")
-        get_emoji("🎭")
-        get_emoji("👩‍⚕️")
+     currencyConverterBtn.addEventListener('click', () => {
+        fetch(urlEffectiveDate)
+         .then((response) => response.json())
+           .then((data) => {
+            let showEffectiveDate = document.querySelector('.effective-date');
+            showEffectiveDate.innerHTML = data[0].effectiveDate;
+             
+            //  showEffectiveDate.value = data[0].effectiveDate;
+            //  const rowEffectiveDate = document.querySelector('.row-effective-date');
+            //  rowEffectiveDate.appendChild(showEffectiveDate);
+     })
+           .catch((err) => console.error('err', err));
+          })
+
+
+
+        //   document.effectiveDate.insertAdjacentHTML("beforeend", `<p>${data.value}</p>`);
+
+    //  fetch(urlthree)
+    //  .then((response) => response.json())
+    //  .then((data) => {(data[0].effectiveDate));
+    //  document.effectiveDate.insertAdjacentHTML("beforeend", `<p>${data.value}</p>`)
+    // .catch((err) => console.error('err', err));
